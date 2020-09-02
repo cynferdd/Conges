12c1a3f040de33ab31ec0059d94f222493a8ece3
Mathieu JOLIVET <m.jolivet@linkbynet.com>
2020-09-02 21:12:52 +0200

    Setting up first acceptance test for Unauthorized authentication

--- Authentication/Authentication.Web.Test/Acceptance.cs
+++ Authentication/Authentication.Web.Test/Acceptance.cs
@@ -1,4 +1,8 @@
+using Authentication.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NFluent;
 using System;
+using System.Net;
 using Xunit;
 
 namespace Authentication.Web.Test
@@ -8,7 +12,11 @@ namespace Authentication.Web.Test
         [Fact]
         public void ShouldReturn410HttpCode_WhenUserNotAuthenticated()
         {
+            var controller = new AuthenticationController();
 
+            var actual = controller.Get(Guid.NewGuid().ToString());
+
+            Check.That(actual).IsInstanceOf<UnauthorizedResult>();
         }
     }
 }
---
+++ Authentication/Authentication.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Web.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        
+        [HttpGet("/{token}")]
+        public IActionResult Get([FromQuery] string token)
+        {
+            return Unauthorized();
+        }
+    }
+
+    
+}
