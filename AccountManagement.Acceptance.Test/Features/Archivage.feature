81134daccef8ae51cc4f6fab666c1c8928807fd7
Mathieu JOLIVET <m.jolivet@linkbynet.com>
2020-10-13 22:15:04 +0200

    Mise en place des tests unitaires avec specflow pour l'archivage

---
+++ AccountManagement.Acceptance.Test/Features/Archivage.feature
@@ -0,0 +1,24 @@
+﻿#language: fr-FR
+Fonctionnalité: Authentification
+  Quand un compte n'est plus désiré, on doit pouvoir l'archiver
+@ArchivageCompte
+Scénario: Archivage de compte inexistant
+  Etant donné un compte qui n'existe pas
+  Quand on l'archive
+  Alors on recoit un code Http NotFound
+
+Scénario: Archivage de compte déjà archivé
+	Etant donné un compte qui existe
+	Et ce compte est déjà archivé
+	Quand on l'archive
+	Alors on recoit un code Http Ok
+	Et Le compte est archivé
+	Et la date d'archivage reste la même
+
+Scénario: Archivage de compte non archivé
+	Etant donné un compte qui existe
+	Et ce compte n'est pas archivé
+	Quand on l'archive
+	Alors on recoit un code Http Ok
+	Et le compte est archivé
+	Et la date d'archivage est la date du jour et l'heure actuelle
---
+++ AccountManagement.Acceptance.Test/Steps/ArchivageSteps.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AccountManagement.Acceptance.Test.Steps
+{
+    [Binding]
+    public class AuthentificationSteps
+    {
+        [Given(@"un compte qui n'existe pas")]
+        public void SoitUnCompteQuiNExistePas()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"un compte qui existe")]
+        public void SoitUnCompteQuiExiste()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"ce compte est déjà archivé")]
+        public void SoitCeCompteEstDejaArchive()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"ce compte n'est pas archivé")]
+        public void SoitCeCompteNEstPasArchive()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"on l'archive")]
+        public void QuandOnLArchive()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"on recoit un code Http NotFound")]
+        public void AlorsOnRecoitUnCodeHttpNotFound()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"on recoit un code Http Ok")]
+        public void AlorsOnRecoitUnCodeHttpOk()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Le compte est archivé")]
+        public void AlorsLeCompteEstArchive()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"la date d'archivage reste la même")]
+        public void AlorsLaDateDArchivageResteLaMeme()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"le compte est archivé")]
+        public void AlorsLeCompteEstArchive()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"la date d'(.*)'heure actuelle")]
+        public void AlorsLaDateDHeureActuelle(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
